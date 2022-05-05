e2d0023bd334daabe4aa7afd0b4f0d19880e650b
NaveenPixeler <cgnaveen022@gmail.com>
2022-05-05 17:26:54 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# angular-ivy-puj8wz
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/angular-ivy-puj8wz)
---
+++ src/app/app.component.ts
@@ -0,0 +1,72 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-app',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  input = '';
+  result = '';
+
+  onNumberClick(num: string) {
+    if (num === '0') {
+      if (!this.input) return;
+    }
+
+    // not to repeat period more than once
+    if (num === '.' && this.input.includes('.')) return;
+
+    this.input += num;
+    this.calculate();
+  }
+
+  onOperatorClick(operator: string) {
+    // without any number not accepting operators
+    if (!this.input) return;
+
+    //Do not allow operators more than once
+    const lastKey = this.input[this.input.length - 1];
+    if (
+      lastKey === '+' ||
+      lastKey === '-' ||
+      lastKey === '*' ||
+      lastKey === '/' ||
+      lastKey === '%'
+    )
+      return;
+
+    this.input += operator;
+    this.calculate();
+  }
+
+  calculate() {
+    if (!this.input) return;
+    if (
+      this.input.includes('+') ||
+      this.input.includes("'-'") ||
+      this.input.includes('*') ||
+      this.input.includes('/') ||
+      this.input.includes('%')
+    ) {
+      let doOperation = this.input;
+      this.result = eval(doOperation);
+    }
+  }
+
+  allClear() {
+    this.input = '';
+    this.result = '';
+  }
+
+  delete() {
+    if (this.input !== '')
+      this.input = this.input.substr(0, this.input.length - 1);
+  }
+
+  onGetResult() {
+    this.calculate();
+    this.input = this.result;
+    this.result = '';
+  }
+}
